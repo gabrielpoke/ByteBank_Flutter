63f87ff3a6ea41ec4fe1c543b5f89c68e0cc54c1
Gabriel Victor <gabriel_victor_prestes@hotmail.com>
2021-05-13 10:51:56 -0300

    Exercise 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,67 @@
 import 'package:flutter/material.dart';
 
+//====================  EXERCICIO 1 CURSO DE FLUTTER ARVORE DE WIDGETS =====================================================================================================
 void main() => runApp(
       Column(
         children: <Widget>[
           Text(
-            'Primeira aula',
+            ' Text 1 ',
             textDirection: TextDirection.ltr,
           ),
           Text(
-            'Exemplo',
+            ' Text 2 ',
             textDirection: TextDirection.ltr,
+          ),
+          Text(
+            'Text 3 ',
+            textDirection: TextDirection.ltr,
+          ),
+          Expanded(
+            child: FittedBox(
+              fit: BoxFit.contain, // otherwise the logo will be tiny
+              child: const FlutterLogo(),
+            ),
           ),
           Column(
             children: <Widget>[
               Text(
-                'Estou aqui',
+                ' Text 4 ',
                 textDirection: TextDirection.ltr,
               ),
             ],
-          ),
-          Expanded(
-            child: FittedBox(
-              fit: BoxFit.contain,
-              child: const FlutterLogo(),
-            ),
-          ),
+          )
         ],
       ),
     );
+//====================  EXERCICIO 1 CURSO DE FLUTTER ARVORE DE WIDGETS =====================================================================================================
+
+
+
+// void main() => runApp(
+//       Column(
+//         children: <Widget>[
+//           Text(
+//             'Primeira aula',
+//             textDirection: TextDirection.ltr,
+//           ),
+//           Text(
+//             'Exemplo',
+//             textDirection: TextDirection.ltr,
+//           ),
+//           Column(
+//             children: <Widget>[
+//               Text(
+//                 'Estou aqui',
+//                 textDirection: TextDirection.ltr,
+//               ),
+//             ],
+//           ),
+//           Expanded(
+//             child: FittedBox(
+//               fit: BoxFit.contain,
+//               child: const FlutterLogo(),
+//             ),
+//           ),
+//         ],
+//       ),
+//     );
