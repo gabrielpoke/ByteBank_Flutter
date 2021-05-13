a800c4a2ce7d52afdaa89f93b72bf2aee986c53d
Gabriel Victor <gabriel_victor_prestes@hotmail.com>
2021-05-13 11:37:09 -0300

    Second class

--- lib/main.dart
+++ lib/main.dart
@@ -1,67 +1,51 @@
 import 'package:flutter/material.dart';
 
 //====================  EXERCICIO 1 CURSO DE FLUTTER ARVORE DE WIDGETS =====================================================================================================
-void main() => runApp(
-      Column(
-        children: <Widget>[
-          Text(
-            ' Text 1 ',
-            textDirection: TextDirection.ltr,
-          ),
-          Text(
-            ' Text 2 ',
-            textDirection: TextDirection.ltr,
-          ),
-          Text(
-            'Text 3 ',
-            textDirection: TextDirection.ltr,
-          ),
-          Expanded(
-            child: FittedBox(
-              fit: BoxFit.contain, // otherwise the logo will be tiny
-              child: const FlutterLogo(),
-            ),
-          ),
-          Column(
-            children: <Widget>[
-              Text(
-                ' Text 4 ',
-                textDirection: TextDirection.ltr,
-              ),
-            ],
-          )
-        ],
-      ),
-    );
-//====================  EXERCICIO 1 CURSO DE FLUTTER ARVORE DE WIDGETS =====================================================================================================
-
-
-
 // void main() => runApp(
 //       Column(
 //         children: <Widget>[
 //           Text(
-//             'Primeira aula',
+//             ' Text 1 ',
 //             textDirection: TextDirection.ltr,
 //           ),
 //           Text(
-//             'Exemplo',
+//             ' Text 2 ',
 //             textDirection: TextDirection.ltr,
+//           ),
+//           Text(
+//             'Text 3 ',
+//             textDirection: TextDirection.ltr,
+//           ),
+//           Expanded(
+//             child: FittedBox(
+//               fit: BoxFit.contain, // otherwise the logo will be tiny
+//               child: const FlutterLogo(),
+//             ),
 //           ),
 //           Column(
 //             children: <Widget>[
 //               Text(
-//                 'Estou aqui',
+//                 ' Text 4 ',
 //                 textDirection: TextDirection.ltr,
 //               ),
 //             ],
-//           ),
-//           Expanded(
-//             child: FittedBox(
-//               fit: BoxFit.contain,
-//               child: const FlutterLogo(),
-//             ),
-//           ),
+//           )
 //         ],
 //       ),
 //     );
+//====================  EXERCICIO 1 CURSO DE FLUTTER ARVORE DE WIDGETS =====================================================================================================
+
+//====================  AULA 2 CURSO DE FLUTTER INICIO DA APLICAÇÃO  =====================================================================================================
+void main() => runApp(
+      MaterialApp(
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text(' Transferências'),
+          ),
+          floatingActionButton: FloatingActionButton(
+            child: Icon(Icons.add),
+          ),
+        ),
+      ),
+    );
+//====================  AULA 2 CURSO DE FLUTTER INICIO DA APLICAÇÃO =====================================================================================================
